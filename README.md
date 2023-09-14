d146283f9f78b826fac6a51085cef3bb30ed6e47
threethan <ethanjmedei@gmail.com>
2023-09-14 05:03:53 -0400

    Update for v5X

--- README.md
+++ README.md
@@ -1,21 +1,13 @@
 # Quest Hidden Settings
 Quickly manage hidden settings on the Quest / Quest 2
 
-![image](https://user-images.githubusercontent.com/12588584/152893080-99122b83-06bb-43c2-ba78-149160c29a2e.png)
-
-The goal of this app is to include every functional and useful settings page and intent on the Quest OS\
-Currently up to date as of v37
-
-Special thanks to [basti564](https://github.com/basti564) for reverse engineering vrshell intents. This app essentially serves as a a combined, user-friendly interface for the Android Settings App (which is very limited on the Quest) and [basti564/vrLauncher](https://github.com/basti564/vrLauncher)
+This app exposes a number of useful settings pages on the Quest that are not otherwise accessible
 
 ## Current Functionality
- - Set brightness to 0 (Quest UI limits range from 16 to 100)
- - Open the hidden full room setup guardian menu (with explaination)
  - Guided setup for Private DNS, with a list of popular servers, including ad-blockers (Adblock in all apps!)
- - Link to Night Light settings pane, allowing schedules to be easily set
  - Link to Date and Time settings, allowing clock to be changed
  - Link to App Settings, allowing apps to be killed and app data/cache to be wiped
  - Link to Android's accessibility settings. High contrast text and mono audio work
  - Link to Device Info, showing build numbers and allowing you to change the device name for bluetooth
 
-All of these features exist, but are inaccessible within the Quest UI
+All of these features exist, but are inaccessible within the normal Quest UI
--- app/src/main/java/com/threethan/questhiddensettings/MainActivity.kt
+++ app/src/main/java/com/threethan/questhiddensettings/MainActivity.kt
@@ -1,80 +1,44 @@
 package com.threethan.questhiddensettings
 
-import android.content.*
-import android.net.Uri
-import android.os.Build
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.provider.Settings
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    var audioApps: Array<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun settingsActivity(intentname: String) {
+        fun settingsActivity(intentName: String) {
             val intent = Intent()
             intent.component =
-                ComponentName("com.android.settings", "com.android.settings.Settings\$"+intentname)
+                ComponentName("com.android.settings", "com.android.settings.Settings\$$intentName")
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
         }
 
-
-        val brightnessBtn = findViewById<Button>(R.id.brightnessBtn)
-        val guardianBtn = findViewById<Button>(R.id.guardianBtn)
         val dnsBtn = findViewById<Button>(R.id.dnsBtn)
-        val nightlightBtn = findViewById<Button>(R.id.nightlightBtn)
         val datetimeBtn = findViewById<Button>(R.id.datetimeBtn)
         val appsBtn = findViewById<Button>(R.id.appsBtn)
         val accessibilityBtn = findViewById<Button>(R.id.accessiblityBtn)
         val infoBtn = findViewById<Button>(R.id.infoBtn)
 
-        brightnessBtn.setOnClickListener {
-            if (Settings.System.canWrite(applicationContext)) {
-                Settings.System.putInt(application.getContentResolver(),
-                Settings.System.SCREEN_BRIGHTNESS, 0)
-            }
-            else {
-                val intent = Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
-                intent.setData(Uri.parse("package:" + applicationContext.packageName));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-        }
-
-        guardianBtn.setOnClickListener {
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("Info and Warning")
-            builder.setMessage(
-                "This menu gives you a lot more control over your home setup with guardian, however not all of these features are complete.\n"+
-                "Additional couches and desks work great, and you can even set the depth of them.\n" +
-                "IF YOU HAVE ANY WALLS SET, DO NOT SET YOUR ENVIRONMENT TO \"STUDIO\". YOUR HOME APP WILL CRASH!\n" +
-                "If anything goes wrong, run the following command through adb: \"adb shell pm clear com.oculus.guardian"
-            )
-            builder.setPositiveButton("I Understand", { _, _ ->
-                val intent = Intent(Intent.ACTION_VIEW)
-                intent.component = ComponentName("com.oculus.vrshell", "com.oculus.vrshell.MainActivity")
-                intent.data = Uri.parse("systemux://guardian/room-capture")
-                this.startActivity(intent)
-            })
-            builder.setNegativeButton("Cancel", {_,_ -> })
-            builder.create().show()
-        }
 
         dnsBtn.setOnClickListener {
-            val items = arrayOf<CharSequence>("Mullvad Adblock (Blocks Ads, Private)", "Mullvad DNS (Private)", "Adguard DNS (Blocks Ads)", "Cloudflare DNS (Fast)", "Quad9 (Private)", "Google DNS")
-            val hostnames = arrayOf<CharSequence>("adblock.doh.mullvad.net", "doh.mullvad.net", "dns.adguard.com", "1dot1dot1dot1.cloudflare-dns.com", "dns.google")
+            val items = arrayOf<CharSequence>("AdGuard DNS (Blocks Ads, Recommended)", "Cloudflare DNS (Fast)", "Quad9 (Private)", "Google DNS")
+            val hostnames = arrayOf<CharSequence>("dns.adguard.com", "1dot1dot1dot1.cloudflare-dns.com", "dns.google")
 
             val builder = AlertDialog.Builder(this)
-            builder.setTitle("Select a DNS provider to copy it's hostname to the clipboard")
+            builder.setTitle("Select a DNS provider")
             builder.setItems(items) { _, item ->
                 val clipBoard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
                 val clipData = ClipData.newPlainText("label",hostnames[item])
@@ -82,24 +46,21 @@ class MainActivity : AppCompatActivity() {
 
                 // Show instructions
                 val builder2 = AlertDialog.Builder(this)
-                builder2.setTitle("Copied hostname!")
+                builder2.setTitle("Copied DNS Provider!")
                 builder2.setMessage(
                     "1. Click next to open network settings\n" +
                     "2. Scroll down, press Advanced, then Private DNS\n" +
-                    "3. Select Private DNS provider hostname. Delete all the text, then press and hold to paste \""+hostnames[item]+"\"\n" +
-                    "4. Press OK and enjoy!"
+                    "3. Select Private DNS provider hostname. Delete all text that may be there, then press and hold to paste \""+hostnames[item]+"\"\n" +
+                    "4. Press OK and enjoy!\n\n" +
+                    "WARNING: Depending on your chosen DNS, you may need to turn this off to sync with your phone"
                 )
-                builder2.setPositiveButton("Next", DialogInterface.OnClickListener { _, _ ->
+                builder2.setPositiveButton("Next") { _, _ ->
                     settingsActivity("NetworkDashboardActivity")
-                })
+                }
                 builder2.create().show()
                 }
             builder.setNegativeButton("Cancel") { _, _ -> }
             builder.create().show()
-        }
-
-        nightlightBtn.setOnClickListener {
-            settingsActivity("NightDisplaySettingsActivity")
         }
 
         datetimeBtn.setOnClickListener {
@@ -121,14 +82,14 @@ class MainActivity : AppCompatActivity() {
             builder.setMessage(
                 "Many of the settings here don't work.\n"+
                 "You can enable mono audio or adjust balance,\n"+
-                "enable high-contrast text system wide,\n"+
-                "or adjust text size without restarting.\n\n"+
-                "Use Oculus's accessibility for working color filters."
+                "enable bold or high-contrast text system wide,\n"+
+                "or adjust text size without being forced to restart.\n\n"+
+                "Use the Quest's accessibility for working color filters."
             )
-            builder.setPositiveButton("Continue", { _, _ ->
+            builder.setPositiveButton("Continue") { _, _ ->
                 settingsActivity("AccessibilitySettingsActivity")
-            })
-            builder.setNegativeButton("Cancel", {_,_ -> })
+            }
+            builder.setNegativeButton("Cancel") { _, _ -> }
             builder.create().show()
         }
 
